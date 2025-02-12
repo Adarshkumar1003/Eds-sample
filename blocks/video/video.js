7b47417f3553863b9927f89a348813b2661e1488
Adarshkumar1003 <aadarsh@adobe.com>
2025-02-12 12:03:54 +0530

    update10

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,24 +1,27 @@
-// Function to embed the video dynamically
-function embedVideo(videoUrl, width = '640', height = '360') {
+function embedGoogleDriveVideo(fileId, width = '640', height = '360') {
+    // Create the direct download link for the video
+    var videoUrl = 'https://drive.google.com/uc?export=download&id=' + fileId;
+
+    // Get the video container where the video will be placed
     var videoContainer = document.getElementById('video-container');
 
     // Create the <video> element
     var videoElement = document.createElement('video');
     videoElement.width = width;
     videoElement.height = height;
-    videoElement.controls = true;  // Adds play, pause, volume controls
+    videoElement.controls = true;  // Add video controls (play, pause, etc.)
 
-    // Create the <source> element
+    // Create the <source> element to link the video URL
     var videoSource = document.createElement('source');
-    videoSource.src = videoUrl;
-    videoSource.type = 'video/mp4'; // Adjust the type based on the video format
+    videoSource.src = https://drive.google.com/file/d/1ZGmN_PuFsYWopVUEA6HgSGso8bBwrfRm/view?usp=drive_link;
+    videoSource.type = 'video/mp4';  // Specify the video format
 
-    // Append the source element to the video element
+    // Append the source to the video element
     videoElement.appendChild(videoSource);
 
-    // Append the video element to the video container
+    // Append the video element to the container
     videoContainer.appendChild(videoElement);
 }
 
-// Example usage: Calling the function with a video URL
-embedVideo('https://drive.google.com/file/d/1ZGmN_PuFsYWopVUEA6HgSGso8bBwrfRm/view?usp=drive_link');
+// Example usage: Embed the video using the Google Drive File ID
+embedGoogleDriveVideo('1ZGmN_PuFsYWopVUEA6HgSGso8bBwrfRm/view?usp=drive_link');
