57a5463e873dbca140b395a0adf1797cb3ac5d10
Adarshkumar1003 <aadarsh@adobe.com>
2025-02-13 05:07:00 +0530

    Update table.js

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -3,28 +3,25 @@ const placeholders = await fetchPlaceholders(getMetadata("locale"));
 
 const { allCountries, abbreviation, africa, america, asia, australia, capital, continent, countries, europe, sNo } = placeholders;
 
-async function createTableHeader(table) {
-    let tr = document.createElement("tr");
-    let sno = document.createElement("th"); sno.textContent = sNo;
-    let country = document.createElement("th"); country.textContent = countries;
-    let continenth = document.createElement("th"); continenth.textContent = continent;
-    let capitalh = document.createElement("th"); capitalh.textContent = capital;
-    let abbr = document.createElement("th"); abbr.textContent = abbreviation;
-
-    tr.append(sno, country, continenth, capitalh, abbr);
-    table.appendChild(tr);
+async function createTableHeader(table){
+    let tr=document.createElement("tr");
+    let sno=document.createElement("th");sno.appendChild(document.createTextNode(sNo));
+    let conuntry=document.createElement("th");conuntry.appendChild(document.createTextNode(countries));
+    let continenth=document.createElement("th");continenth.appendChild(document.createTextNode(continent));
+    let capitalh=document.createElement("th");capitalh.appendChild(document.createTextNode(capital));
+    let abbr=document.createElement("th");abbr.appendChild(document.createTextNode(abbreviation));
+    tr.append(sno);tr.append(conuntry);tr.append(capitalh);tr.append(continenth);tr.append(abbr);
+    table.append(tr);
 }
-
-async function createTableRow(table, row, i) {
-    let tr = document.createElement("tr");
-    let sno = document.createElement("td"); sno.textContent = i;
-    let country = document.createElement("td"); country.textContent = row.Country;
-    let continent = document.createElement("td"); continent.textContent = row.Continent;
-    let capital = document.createElement("td"); capital.textContent = row.Capital;
-    let abbr = document.createElement("td"); abbr.textContent = row.Abbreviation;
-
-    tr.append(sno, country, continent, capital, abbr);
-    table.appendChild(tr);
+async function createTableRow(table,row,i){
+    let tr=document.createElement("tr");
+    let sno=document.createElement("td");sno.appendChild(document.createTextNode(i));
+    let conuntry=document.createElement("td");conuntry.appendChild(document.createTextNode(row.Country));
+    let continent=document.createElement("td");continent.appendChild(document.createTextNode(row.Capital));
+    let capital=document.createElement("td");capital.appendChild(document.createTextNode(row.Continent));
+    let abbr=document.createElement("td");abbr.appendChild(document.createTextNode(row.Abbreviation));
+    tr.append(sno);tr.append(conuntry);tr.append(continent);tr.append(capital);tr.append(abbr);
+    table.append(tr);
 }
 
 async function createTable(jsonURL, val) {
